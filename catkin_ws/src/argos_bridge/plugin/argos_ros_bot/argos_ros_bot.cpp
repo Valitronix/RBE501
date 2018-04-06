d9844963abb903facabd37dedfab14ef7bceb206
Lening Li <xiao00li6868@gmail.com>
2018-04-06 16:21:40 -0400

    added ten robot file

--- catkin_ws/src/argos_bridge/plugin/argos_ros_bot/argos_ros_bot.cpp
+++ catkin_ws/src/argos_bridge/plugin/argos_ros_bot/argos_ros_bot.cpp
@@ -1,106 +1,260 @@
-// ROS Stuff #include "ros/ros.h"
-#include "argos_bridge/Haptic.h"
-#include "argos_bridge/State.h"
-
 /* Include the controller definition */
 #include "argos_ros_bot.h"
 /* Function definitions for XML parsing */
 #include <argos3/core/utility/configuration/argos_configuration.h>
-/* 2D vector definition */
-#include <argos3/core/utility/math/vector2.h>
+#include <argos3/core/utility/logging/argos_log.h>
 
 #include <iostream>
 #include <sstream>
-
-#include <ros/callback_queue.h>
-
+#include <math.h>
 using namespace std;
 using namespace argos_bridge;
+#define PI 3.14159265
 
 // Initialize ROS node.  There will be only one ROS node no matter how many robots are created in
 // ARGoS.  However, we will have one instance of the CArgosRosBot class for each ARGoS robot.
 ros::NodeHandle* initROS() {
   int argc = 0;
   char *argv = (char *) "";
-  ros::init(argc, &argv, "argos_bridge");
+  ros::init(argc, &argv, "argos_ros_bot");
   return new ros::NodeHandle();
 }
 
-ros::NodeHandle* CArgosRosBot::nodeHandle = initROS();
+ros::NodeHandle* CFootBotFlocking::nodeHandle = initROS();
+
 
 /****************************************/
 /****************************************/
 
-CArgosRosBot::CArgosRosBot() :
-  m_pcWheels(NULL),
-  m_pcState(NULL)
-{
+void CFootBotFlocking::SWheelTurningParams::Init(TConfigurationNode& t_node) {
+   try {
+      TurningMechanism = NO_TURN;
+      CDegrees cAngle;
+      GetNodeAttribute(t_node, "hard_turn_angle_threshold", cAngle);
+      HardTurnOnAngleThreshold = ToRadians(cAngle);
+      GetNodeAttribute(t_node, "soft_turn_angle_threshold", cAngle);
+      SoftTurnOnAngleThreshold = ToRadians(cAngle);
+      GetNodeAttribute(t_node, "no_turn_angle_threshold", cAngle);
+      NoTurnAngleThreshold = ToRadians(cAngle);
+      GetNodeAttribute(t_node, "max_speed", MaxSpeed);
+   }
+   catch(CARGoSException& ex) {
+      THROW_ARGOSEXCEPTION_NESTED("Error initializing controller wheel turning parameters.", ex);
+   }
 }
 
-void CArgosRosBot::Init(TConfigurationNode& t_node) {
+/****************************************/
+/****************************************/
 
-  try {
-   /* Wheel turning */
-   m_sWheelTurningParams.Init(GetNode(t_node, "wheel_turning"));
-   /* Flocking-related */
-  }
-    catch(CARGoSException& ex) {
-    THROW_ARGOSEXCEPTION_NESTED("Error parsing the controller parameters.", ex);
-  }
-  // Create the topics to publish
-  stringstream stateTopic;
-  stateTopic << "/" << GetId() << "/state";
-
-  statePub = nodeHandle->advertise<State>(stateTopic.str(), 1000);
-
-  // Create the subscribers
-  stringstream hapticTopic;
-  hapticTopic << "/" << GetId() << "/haptic";
-
-  hapticSub = nodeHandle->subscribe(hapticTopic.str(), 1000, &CArgosRosBot::hapticCallback, this);
-
-  // Get sensor/actuator handles
-  m_pcWheels = GetActuator<CCI_DifferentialSteeringActuator>("differential_steering");
-  m_pcState = GetSensor<CCI_PositioningSensor>("positioning");
+void CFootBotFlocking::SFlockingInteractionParams::Init(TConfigurationNode& t_node) {
+   try {
+      GetNodeAttribute(t_node, "target_distance", TargetDistance);
+      GetNodeAttribute(t_node, "gain", Gain);
+      GetNodeAttribute(t_node, "exponent", Exponent);
+   }
+   catch(CARGoSException& ex) {
+      THROW_ARGOSEXCEPTION_NESTED("Error initializing controller flocking parameters.", ex);
+   }
 }
 
-void CArgosRosBot::ControlStep() {
+/****************************************/
+/****************************************/
 
-  /* Get readings from positioning sensor */
-  const CCI_PositioningSensor::SReading& sStateReads = m_pcState->GetReading();
-  State state;
-  state.x = sStateReads.Position.GetX();
-  state.y = sStateReads.Position.GetY();
-  state.z = sStateReads.Position.GetZ();
-  state.dot_x = 0;
-  state.dot_y = 0;
-  state.dot_z = 0;
-
-  statePub.publish(state);
-  // cout << "Lening Li is awesome!!!!!"<<endl;
-
-  // Wait for any callbacks to be called.
-  ros::getGlobalCallbackQueue()->callAvailable(ros::WallDuration(0.1));
-
-  CVector2 forceVector(xForce, yForce);
-
-  SetWheelSpeedsFromVector(forceVector);
+/*
+ * This function is a generalization of the Lennard-Jones potential
+ */
+Real CFootBotFlocking::SFlockingInteractionParams::GeneralizedLennardJones(Real f_distance) {
+   Real fNormDistExp = ::pow(TargetDistance / f_distance, Exponent);
+   return -Gain / f_distance * (fNormDistExp * fNormDistExp - fNormDistExp);
 }
 
-void CArgosRosBot::hapticCallback(const argos_bridge::Haptic& haptic) {
-  cout << "hapticCallback" << GetId() << endl;
+/****************************************/
+/****************************************/
 
-  xForce = haptic.x_value;
-  yForce = haptic.y_value;
-  zForce = haptic.z_value;
+CFootBotFlocking::CFootBotFlocking() :
+   m_pcWheels(NULL),
+   m_pcLight(NULL),
+   m_pcLEDs(NULL),
+   m_pcCamera(NULL),
+   m_pcState(NULL) {}
+
+/****************************************/
+/****************************************/
+
+void CFootBotFlocking::Init(TConfigurationNode& t_node) {
+   /*
+    * Get sensor/actuator handles
+    *
+    * The passed string (ex. "differential_steering") corresponds to the XML tag of the
+    * device whose handle we want to have. For a list of allowed values, type at the
+    * command prompt:
+    *
+    * $ argos3 -q actuators
+    *
+    * to have a list of all the possible actuators, or
+    *
+    * $ argos3 -q sensors
+    *
+    * to have a list of all the possible sensors.
+    *
+    * NOTE: ARGoS creates and initializes actuators and sensors internally, on the basis of
+    *       the lists provided the configuration file at the
+    *       <controllers><footbot_diffusion><actuators> and
+    *       <controllers><footbot_diffusion><sensors> sections. If you forgot to
+    *       list a device in the XML and then you request it here, an error occurs.
+    */
+
+    // Create the topics to publish
+    stringstream stateTopic;
+    stateTopic << "/" << GetId() << "/State";
+
+    statePub = nodeHandle->advertise<State>(stateTopic.str(), 1000);
+
+    // Create the subscribers
+    stringstream flockingTopic;
+    flockingTopic << "/Flocking";
+
+    flockingSub = nodeHandle->subscribe(flockingTopic.str(), 1000, &CFootBotFlocking::flockingCallback, this);
+
+   m_pcWheels = GetActuator<CCI_DifferentialSteeringActuator          >("differential_steering");
+   m_pcLight  = GetSensor  <CCI_FootBotLightSensor                    >("footbot_light");
+   m_pcLEDs   = GetActuator<CCI_LEDsActuator                          >("leds");
+   m_pcCamera = GetSensor  <CCI_ColoredBlobOmnidirectionalCameraSensor>("colored_blob_omnidirectional_camera");
+   m_pcState = GetSensor<CCI_PositioningSensor>("positioning");
+   /*
+    * Parse the config file
+    */
+   try {
+      /* Wheel turning */
+      m_sWheelTurningParams.Init(GetNode(t_node, "wheel_turning"));
+      /* Flocking-related */
+      m_sFlockingParams.Init(GetNode(t_node, "flocking"));
+   }
+   catch(CARGoSException& ex) {
+      THROW_ARGOSEXCEPTION_NESTED("Error parsing the controller parameters.", ex);
+   }
+
+   /*
+    * Other init stuff
+    */
+   /* Enable camera filtering */
+   m_pcCamera->Enable();
+   /* Set beacon color to all red to be visible for other robots */
+   m_pcLEDs->SetSingleColor(12, CColor::RED);
+}
+
+/****************************************/
+/****************************************/
+
+void CFootBotFlocking::flockingCallback(const argos_bridge::Flocking& flocking) {
+  cout << "flockCallback" << GetId() << endl;
+
+      x = flocking.x;
+      y = flocking.y;
+      distance = flocking.distance;
 
 }
 
-void CArgosRosBot::SetWheelSpeedsFromVector(const CVector2& c_heading) {
+void CFootBotFlocking::ControlStep() {
 
-    // std::cout << "Enter SetWheelSpeedsFromVector function" << '\n';
-    // std::cout << c_heading.GetX() << '\n';
-    // std::cout << c_heading.GetY() << '\n';
+   /* Get readings from positioning sensor */
+   const CCI_PositioningSensor::SReading& sStateReads = m_pcState->GetReading();
+   State state;
+   state.x = sStateReads.Position.GetX();
+   state.y = sStateReads.Position.GetY();
+   state.z = sStateReads.Position.GetZ();
+   state.dot_x = 0;
+   state.dot_y = 0;
+   state.dot_z = 0;
+
+   statePub.publish(state);
+
+   SetWheelSpeedsFromVector(VectorToLight(1, PI/4) + FlockingVector());
+
+}
+
+/****************************************/
+/****************************************/
+
+CVector2 CFootBotFlocking::VectorToLight(Real x, Real y) {
+   /* Get light readings */
+   const CCI_FootBotLightSensor::TReadings& tReadings = m_pcLight->GetReadings();
+   /* Calculate a normalized vector that points to the closest light */
+   CVector2 cAccum;
+   for(size_t i = 0; i < tReadings.size(); ++i) {
+      cAccum += CVector2(x, y);
+   }
+   cAccum = CVector2(x, y);
+   cout << cAccum << endl;
+   if(cAccum.Length() > 0.0f) {
+      /* Make the vector long as 1/4 of the max speed */
+      cAccum.Normalize();
+      cAccum *= 0.25f * m_sWheelTurningParams.MaxSpeed;
+   }
+   return cAccum;
+}
+
+/****************************************/
+/****************************************/
+
+CVector2 CFootBotFlocking::FlockingVector() {
+   /* Get the camera readings */
+   const CCI_ColoredBlobOmnidirectionalCameraSensor::SReadings& sReadings = m_pcCamera->GetReadings();
+   /* Go through the camera readings to calculate the flocking interaction vector */
+   if(! sReadings.BlobList.empty()) {
+      CVector2 cAccum;
+      Real fLJ;
+      size_t unBlobsSeen = 0;
+
+      for(size_t i = 0; i < sReadings.BlobList.size(); ++i) {
+
+         /*
+          * The camera perceives the light as a yellow blob
+          * The robots have their red beacon on
+          * So, consider only red blobs
+          * In addition: consider only the closest neighbors, to avoid
+          * attraction to the farthest ones. Taking 180% of the target
+          * distance is a good rule of thumb.
+          */
+         if(sReadings.BlobList[i]->Color == CColor::RED &&
+            sReadings.BlobList[i]->Distance < m_sFlockingParams.TargetDistance * 1.80f) {
+            /*
+             * Take the blob distance and angle
+             * With the distance, calculate the Lennard-Jones interaction force
+             * Form a 2D vector with the interaction force and the angle
+             * Sum such vector to the accumulator
+             */
+            /* Calculate LJ */
+            fLJ = m_sFlockingParams.GeneralizedLennardJones(sReadings.BlobList[i]->Distance);
+            /* Sum to accumulator */
+            cAccum += CVector2(fLJ,
+                               sReadings.BlobList[i]->Angle);
+            /* Increment the blobs seen counter */
+            ++unBlobsSeen;
+         }
+      }
+      if(unBlobsSeen > 0) {
+         /* Divide the accumulator by the number of blobs seen */
+         cAccum /= unBlobsSeen;
+         /* Clamp the length of the vector to the max speed */
+         if(cAccum.Length() > m_sWheelTurningParams.MaxSpeed) {
+            cAccum.Normalize();
+            cAccum *= m_sWheelTurningParams.MaxSpeed;
+         }
+         return cAccum;
+      }
+      else
+         return CVector2();
+   }
+   else {
+      return CVector2();
+   }
+}
+
+/****************************************/
+/****************************************/
+
+void CFootBotFlocking::SetWheelSpeedsFromVector(const CVector2& c_heading) {
    /* Get the heading angle */
    CRadians cHeadingAngle = c_heading.Angle().SignedNormalize();
    /* Get the length of the heading vector */
@@ -164,39 +318,18 @@ void CArgosRosBot::SetWheelSpeedsFromVector(const CVector2& c_heading) {
       fLeftWheelSpeed  = fSpeed2;
       fRightWheelSpeed = fSpeed1;
    }
-   // std::cout << "left wheel speed" << '\n';
-   // std::cout << fLeftWheelSpeed << '\n';
-   // std::cout << "right wheel speed" << '\n';
-   // std::cout << fRightWheelSpeed << '\n';
    /* Finally, set the wheel speeds */
    m_pcWheels->SetLinearVelocity(fLeftWheelSpeed, fRightWheelSpeed);
 }
 
-void CArgosRosBot::SWheelTurningParams::Init(TConfigurationNode& t_node) {
-   try {
-      TurningMechanism = NO_TURN;
-      CDegrees cAngle;
-      GetNodeAttribute(t_node, "hard_turn_angle_threshold", cAngle);
-      HardTurnOnAngleThreshold = ToRadians(cAngle);
-      GetNodeAttribute(t_node, "soft_turn_angle_threshold", cAngle);
-      SoftTurnOnAngleThreshold = ToRadians(cAngle);
-      GetNodeAttribute(t_node, "no_turn_angle_threshold", cAngle);
-      NoTurnAngleThreshold = ToRadians(cAngle);
-      GetNodeAttribute(t_node, "max_speed", MaxSpeed);
-   }
-   catch(CARGoSException& ex) {
-      THROW_ARGOSEXCEPTION_NESTED("Error initializing controller wheel turning parameters.", ex);
-   }
-}
+/****************************************/
+/****************************************/
 
 /*
-* This statement notifies ARGoS of the existence of the controller.
-* It binds the class passed as first argument to the string passed as
-* second argument.
-* The string is then usable in the configuration file to refer to this
-* controller.
- * When ARGoS reads that string in the configuration file, it knows which
- * controller class to instantiate.
- * See also the configuration files for an example of how this is used.
+ * This statement notifies ARGoS of the existence of the controller.
+ * It binds the class passed as first argument to the string passed as second argument.
+ * The string is then usable in the XML configuration file to refer to this controller.
+ * When ARGoS reads that string in the XML file, it knows which controller class to instantiate.
+ * See also the XML configuration files for an example of how this is used.
  */
-REGISTER_CONTROLLER(CArgosRosBot, "argos_ros_bot_controller")
+REGISTER_CONTROLLER(CFootBotFlocking, "argos_ros_bot_controller")
--- catkin_ws/src/argos_bridge/plugin/argos_ros_bot/argos_ros_bot.h
+++ catkin_ws/src/argos_bridge/plugin/argos_ros_bot/argos_ros_bot.h
@@ -1,125 +1,190 @@
 /*
- * AUTHOR: Andrew Vardy <av@mun.ca>
+ * AUTHOR: Carlo Pinciroli <cpinciro@ulb.ac.be>
  *
- * Connects an ARGoS robot with a particular configuration to ROS by publishing
- * sensor values and subscribing to a desired wheel speeds topic.
+ * An example flocking controller for the foot-bot.
  *
+ * This controller lets a group of foot-bots flock in an hexagonal lattice towards
+ * a light source placed in the arena. To flock, it exploits a generalization of the
+ * well known Lennard-Jones potential. The parameters of the Lennard-Jones function
+ * were chosen through a simple trial-and-error procedure on its graph.
+ *
+ * This controller is meant to be used with the XML file:
+ *    experiments/flocking.argos
  */
 
-#ifndef ARGOS_ROS_BOT_H
-#define ARGOS_ROS_BOT_H
+#ifndef FOOTBOT_FLOCKING_H
+#define FOOTBOT_FLOCKING_H
 
+/*
+ * Include some necessary headers.
+ */
+/* Definition of the CCI_Controller class. */
 #include <argos3/core/control_interface/ci_controller.h>
+/* Definition of the differential steering actuator */
 #include <argos3/plugins/robots/generic/control_interface/ci_differential_steering_actuator.h>
+/* Definition of the LEDs actuator */
+#include <argos3/plugins/robots/generic/control_interface/ci_leds_actuator.h>
+/* Definition of the omnidirectional camera sensor */
+#include <argos3/plugins/robots/generic/control_interface/ci_colored_blob_omnidirectional_camera_sensor.h>
+/* Definition of the foot-bot light sensor */
+#include <argos3/plugins/robots/foot-bot/control_interface/ci_footbot_light_sensor.h>
 #include <argos3/plugins/robots/generic/control_interface/ci_positioning_sensor.h>
+/* Vector2 definitions */
+#include <argos3/core/utility/math/vector2.h>
 
 #include <ros/ros.h>
-#include <string>
-#include "geometry_msgs/Twist.h"
-#include "std_msgs/Bool.h"
+#include <ros/callback_queue.h>
 
+/* Messages definations */
+#include "argos_bridge/Flocking.h"
+#include "argos_bridge/State.h"
+
+/*
+ * All the ARGoS stuff in the 'argos' namespace.
+ * With this statement, you save typing argos:: every time.
+ */
 using namespace argos;
 
-class CArgosRosBot : public CCI_Controller {
+/*
+ * A controller is simply an implementation of the CCI_Controller class.
+ */
+class CFootBotFlocking : public CCI_Controller {
 
 public:
-  struct SWheelTurningParams {
+
    /*
-    * The turning mechanism.
-    * The robot can be in three different turning states.
+    * The following variables are used as parameters for
+    * turning during navigation. You can set their value
+    * in the <parameters> section of the XML configuration
+    * file, under the
+    * <controllers><footbot_flocking_controller><parameters><wheel_turning>
+    * section.
     */
-   enum ETurningMechanism
-   {
-      NO_TURN = 0, // go straight
-      SOFT_TURN,   // both wheels are turning forwards, but at different speeds
-      HARD_TURN    // wheels are turning with opposite speeds
-   } TurningMechanism;
+   struct SWheelTurningParams {
+      /*
+       * The turning mechanism.
+       * The robot can be in three different turning states.
+       */
+      enum ETurningMechanism
+      {
+         NO_TURN = 0, // go straight
+         SOFT_TURN,   // both wheels are turning forwards, but at different speeds
+         HARD_TURN    // wheels are turning with opposite speeds
+      } TurningMechanism;
+      /*
+       * Angular thresholds to change turning state.
+       */
+      CRadians HardTurnOnAngleThreshold;
+      CRadians SoftTurnOnAngleThreshold;
+      CRadians NoTurnAngleThreshold;
+      /* Maximum wheel speed */
+      Real MaxSpeed;
+
+      void Init(TConfigurationNode& t_tree);
+   };
+
    /*
-    * Angular thresholds to change turning state.
+    * The following variables are used as parameters for
+    * flocking interaction. You can set their value
+    * in the <parameters> section of the XML configuration
+    * file, under the
+    * <controllers><footbot_flocking_controller><parameters><flocking>
+    * section.
     */
-   CRadians HardTurnOnAngleThreshold;
-   CRadians SoftTurnOnAngleThreshold;
-   CRadians NoTurnAngleThreshold;
-   /* Maximum wheel speed */
-   Real MaxSpeed;
+   struct SFlockingInteractionParams {
+      /* Target robot-robot distance in cm */
+      Real TargetDistance;
+      /* Gain of the Lennard-Jones potential */
+      Real Gain;
+      /* Exponent of the Lennard-Jones potential */
+      Real Exponent;
 
-   void Init(TConfigurationNode& t_tree);
- };
+      void Init(TConfigurationNode& t_node);
+      Real GeneralizedLennardJones(Real f_distance);
+   };
 
-  CArgosRosBot();
-  virtual ~CArgosRosBot() {}
+public:
 
-  /*
-   * This function initializes the controller.
-   * The 't_node' variable points to the <parameters> section in the XML
-   * file in the <controllers><footbot_ccw_wander_controller> section.
-   */
-  virtual void Init(TConfigurationNode& t_node);
+   /* Class constructor. */
+   CFootBotFlocking();
 
-  /*
-   * This function is called once every time step.
-   * The length of the time step is set in the XML file.
-   */
-  virtual void ControlStep();
+   /* Class destructor. */
+   virtual ~CFootBotFlocking() {}
 
-  /*
-   * This function resets the controller to its state right after the
-   * Init().
-   * It is called when you press the reset button in the GUI.
-   * In this example controller there is no need for resetting anything,
-   * so the function could have been omitted. It's here just for
-   * completeness.
-   */
-  virtual void Reset() {}
+   /*
+    * This function initializes the controller.
+    * The 't_node' variable points to the <parameters> section in the XML file
+    * in the <controllers><footbot_flocking_controller> section.
+    */
+   virtual void Init(TConfigurationNode& t_node);
 
-  /*
-   * Called to cleanup what done by Init() when the experiment finishes.
-   * In this example controller there is no need for clean anything up,
-   * so the function could have been omitted. It's here just for
-   * completeness.
-   */
-  virtual void Destroy() {}
+   /*
+    * This function is called once every time step.
+    * The length of the time step is set in the XML file.
+    */
+   virtual void ControlStep();
 
-  /*
-   * The callback method for getting haptic force vector on the haptic topic.
-   */
-  void hapticCallback(const argos_bridge::Haptic& haptic);
+   /*
+    * This function resets the controller to its state right after the Init().
+    * It is called when you press the reset button in the GUI.
+    * In this example controller there is no need for resetting anything, so
+    * the function could have been omitted. It's here just for completeness.
+    */
+   virtual void Reset() {}
 
+   /*
+    * Called to cleanup what done by Init() when the experiment finishes.
+    * In this example controller there is no need for clean anything up, so
+    * the function could have been omitted. It's here just for completeness.
+    */
+   virtual void Destroy() {}
 
-  void SetWheelSpeedsFromVector(const CVector2& c_heading);
+    void flockingCallback(const argos_bridge::Flocking& flocking);
+
+    // flocking subscriber
+    ros::Subscriber flockingSub;
+
+    // state publisher
+    ros::Publisher statePub;
+
+    Real x, y, distance;
+
+    static ros::NodeHandle* nodeHandle;
+
+protected:
+
+   /*
+    * Calculates the vector to the closest light.
+    */
+   virtual CVector2 VectorToLight(Real x, Real y);
+
+   /*
+    * Calculates the flocking interaction vector.
+    */
+   virtual CVector2 FlockingVector();
+
+   /*
+    * Gets a direction vector as input and transforms it into wheel actuation.
+    */
+   void SetWheelSpeedsFromVector(const CVector2& c_heading);
 
 private:
 
-  CCI_DifferentialSteeringActuator* m_pcWheels;
-  CCI_PositioningSensor* m_pcState;
+   /* Pointer to the differential steering actuator */
+   CCI_DifferentialSteeringActuator* m_pcWheels;
+   /* Pointer to the foot-bot light sensor */
+   CCI_FootBotLightSensor* m_pcLight;
+   /* Pointer to the LEDs actuator */
+   CCI_LEDsActuator* m_pcLEDs;
+   /* Pointer to the omnidirectional camera sensor */
+   CCI_ColoredBlobOmnidirectionalCameraSensor* m_pcCamera;
 
-  // The following constant values were copied from the argos source tree from
-  // the file src/plugins/robots/foot-bot/simulator/footbot_entity.cpp
-  static constexpr Real HALF_BASELINE = 0.07f; // Half the distance between wheels
-  static constexpr Real WHEEL_RADIUS = 0.029112741f;
+   CCI_PositioningSensor* m_pcState;
 
-  /*
-   * The following variables are used as parameters for the
-   * algorithm. You can set their value in the <parameters> section
-   * of the XML configuration file, under the
-   * <controllers><argos_ros_bot_controller> section.
-   */
-
-  Real xForce, yForce, zForce;
-
-  // haptic sensor subscriber
-  ros::Subscriber hapticSub;
-
-  // state publisher
-  ros::Publisher statePub;
-
-  /* The turning parameters. */
-  SWheelTurningParams m_sWheelTurningParams;
-
-public:
-  // We need only a single ROS node, although there are individual publishers
-  // and subscribers for each instance of the class.
-  static ros::NodeHandle* nodeHandle;
+   /* The turning parameters. */
+   SWheelTurningParams m_sWheelTurningParams;
+   /* The flocking interaction parameters. */
+   SFlockingInteractionParams m_sFlockingParams;
 };
 
 #endif
